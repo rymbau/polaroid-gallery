14ee83ccd5b7f065db5f54ffe096bc219d33f62a
Rym BOUCHAGOUR <rym.bouchagour@glicer.com>
2016-07-30 16:26:37 +0200

    center image selected

--- js/polaroid-gallery.js
+++ js/polaroid-gallery.js
@@ -1,6 +1,35 @@
 var polaroidGallery = (function () {
     function polaroidGallery(elements) {
         elements = [].slice.call(elements);
+        var dataSize = [];
+        elements.forEach(function (item) {
+            dataSize.push({width: item.offsetWidth, height: item.offsetHeight});
+
+            item.addEventListener('click', function () {
+                shuffle(elements);
+
+                var idx = (item.id).replace('fig', '') - 1;
+                var scale = 1.8;
+                var rotRandomD = 0;
+
+                var newWidth = (dataSize[idx].width * scale);
+                var newHeight = dataSize[idx].height * (newWidth / dataSize[idx].width);
+                
+                var x = (window.innerWidth - newWidth) / 2;
+                var y = (window.innerHeight - newHeight) / 2;
+
+                item.style.transformOrigin  = '0 0';
+                item.style.WebkitTransform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg) scale(' + scale + ',' + scale + ')';
+                item.style.msTransform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg) scale(' + scale + ',' + scale + ')';
+                item.style.transform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg) scale(' + scale + ',' + scale + ')';
+                item.style.zIndex = 999;
+            })
+
+        });
+    }
+
+    function shuffle(elements) {
+        var zIndex = 1;
         elements.forEach(function (item) {
             var randomX = Math.random();
             var randomY = Math.random();
@@ -15,11 +44,11 @@ var polaroidGallery = (function () {
             var x = Math.floor((window.innerWidth - rotatedW) * randomX);
             var y = Math.floor((window.innerHeight - rotatedH) * randomY);
 
-            item.style.WebkitTransform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg)';
-            item.style.msTransform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg)';
-            item.style.transform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg)';
-
-        });
+            item.style.WebkitTransform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg) scale(1)';
+            item.style.msTransform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg) scale(1)';
+            item.style.transform = 'translate(' + x + 'px,' + y + 'px) rotate(' + rotRandomD + 'deg) scale(1)';
+            item.style.zIndex = zIndex++;
+        })
     }
 
     return polaroidGallery;
